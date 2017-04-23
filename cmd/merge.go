27fb01540f80e2b3df093e3b22212b4fce603f59
alexhokl <alexhokl@gmail.com>
2017-04-23 19:22:23 +0800

    Added stub of merge command

---
+++ cmd/merge.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// mergeCmd represents the merge command
+var mergeCmd = &cobra.Command{
+	Use:   "merge",
+	Short: "Merges one or more PDF files into one",
+	Long:  "Merges one or more PDF files into one",
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("merge called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(mergeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// mergeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// mergeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
