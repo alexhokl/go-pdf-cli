59dff5b4b9c6a30535e15bf46f21f3e467f23279
alexhokl <alexhokl@gmail.com>
2017-04-23 20:09:07 +0800

    Implemented flags of merge command

--- cmd/merge.go
+++ cmd/merge.go
@@ -6,28 +6,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// mergeCmd represents the merge command
-var mergeCmd = &cobra.Command{
-	Use:   "merge",
-	Short: "Merges one or more PDF files into one",
-	Long:  "Merges one or more PDF files into one",
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("merge called")
-	},
+type mergeOptions struct {
+	outputPath string
+	inputPaths []string
 }
 
 func init() {
+	opts := mergeOptions{}
+
+	// mergeCmd represents the merge command
+	mergeCmd := &cobra.Command{
+		Use:   "merge",
+		Short: "Merges one or more PDF files into one",
+		Long:  "Merges one or more PDF files into one",
+		Run: func(cmd *cobra.Command, args []string) {
+			runMerge(opts)
+		},
+	}
+
+	emptyStringArray := []string{}
+
+	flags := mergeCmd.Flags()
+	flags.StringVarP(&opts.outputPath, "output", "o", "", "Output PDF path")
+	flags.StringArrayVarP(&opts.inputPaths, "input", "i", emptyStringArray, "Input path(s)")
+
 	RootCmd.AddCommand(mergeCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// mergeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// mergeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+func runMerge(opts mergeOptions) {
+	fmt.Println("Merge called", opts.outputPath, opts.inputPaths)
 }
